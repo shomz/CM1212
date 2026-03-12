7adfadb8ec7dc7a68cc32cb5b4eb385c022aa2c3
shomz <shomy83@gmail.com>
2026-03-12 10:57:13 +0100

    UI mostly finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,8 @@ public class GameManager : Singleton<GameManager>
             new(5, 6)
         };
 
-        UI.BuildGameTypeButtons(gameTypes);
+        UI.ShowCanvas(UI.HomeCanvas);
+        UI.BuildGameTypeButtons(gameTypes, StartGame);
     }
 
     // Update is called once per frame
@@ -26,12 +27,41 @@ public class GameManager : Singleton<GameManager>
         
     }
 
+    public void ShowGameTypes()
+    {
+        UI.ShowCanvas(UI.TypeCanvas);
+    }
+
     public void StartGame(GameType gameType)
     {
         var grid = CardContainer.GetComponent<GridLayoutGroup>();
         grid.constraintCount = gameType.rows;
+        
+        var spacingCoef = 0.2f;
+        var rectTransform = CardContainer.GetComponent<RectTransform>();
+        var fittingCoef = Mathf.Min(rectTransform.sizeDelta.x / gameType.rows, rectTransform.sizeDelta.y / gameType.cols);
+
+        grid.cellSize = Vector2.one * (fittingCoef * (1 - spacingCoef));
+        grid.spacing = Vector2.one * (fittingCoef * spacingCoef);
+
+        ClearCardContainer();
 
         SpawnCards(gameType.rows * gameType.cols);
+
+        UI.ShowCanvas(UI.GameCanvas);
+    }
+
+    public void EndGame()
+    {
+        UI.ShowCanvas(UI.HomeCanvas);
+    }
+
+    private void ClearCardContainer()
+    {
+        for (int i = CardContainer.childCount - 1; i >= 0; i--)
+        {
+            Destroy(CardContainer.GetChild(i).gameObject);
+        }
     }
 
     private void SpawnCards(int count)
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,10 +1,38 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    internal void BuildGameTypeButtons(GameType[] gameTypes)
+    public Canvas HomeCanvas;
+    public Canvas TypeCanvas;
+    public Canvas GameCanvas;
+    private Canvas[] AllCanvases;
+    [SerializeField] private Button ButtonPrefab;
+
+    private void Start()
     {
-        throw new NotImplementedException();
+        AllCanvases = new Canvas[] { HomeCanvas, TypeCanvas, GameCanvas };
+    }
+
+    public void ShowCanvas(Canvas canvas)
+    {
+        foreach (var c in AllCanvases)
+        {
+            c.gameObject.SetActive(c == canvas);
+        }
+    }
+
+    public void BuildGameTypeButtons(GameType[] gameTypes, Action<GameType> startGame)
+    {
+        foreach (var gameType in gameTypes)
+        {
+            var button = Instantiate(ButtonPrefab, TypeCanvas.transform);
+            var text = button.GetComponentInChildren<TMP_Text>();
+            text.text = $"{gameType.rows}X{gameType.cols}";
+
+            button.onClick.AddListener(() => startGame(gameType));
+        }
     }
 }
