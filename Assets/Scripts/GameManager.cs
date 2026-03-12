73b15449329bdd5fc30780bc62b56aec9a38e5af
shomz <shomy83@gmail.com>
2026-03-12 18:55:01 +0100

    WIP

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +8,7 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private UIController UI;
     [SerializeField] private Transform CardContainer;
     [SerializeField] private CardController CardPrefab;
+    [SerializeField] private int CardTypesCount = 4;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -66,9 +69,33 @@ public class GameManager : Singleton<GameManager>
 
     private void SpawnCards(int count)
     {
+        var ids = GenerateIds(count);
+
         for (int i = 0; i < count; i++)
         {
-            Instantiate(CardPrefab, CardContainer);
+            var card = Instantiate(CardPrefab, CardContainer);
+
+            card.Id = ids[i];
         }
+    }
+
+    private List<int> GenerateIds(int count)
+    {
+        List<int> output = new();
+        var lastId = -1;
+        var id = -1;
+        for (int i = 0; i < count / 2; i++)
+        {
+            while (id == lastId)
+            {
+                id = Random.Range(0, CardTypesCount);
+            }
+            output.Add(id);
+            lastId = id;
+        }
+
+        output.AddRange(output);
+        Debug.Log(JsonUtility.ToJson(output));
+        return output.OrderBy(item => System.Guid.NewGuid()).ToList();
     }
 }
